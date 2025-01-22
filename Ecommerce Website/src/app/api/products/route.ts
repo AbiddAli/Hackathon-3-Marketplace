7ccf51ef55c4636f90d07943dfd922379b39624d
Abid Ali <160251628+AbiddAli@users.noreply.github.com>
2025-01-22 08:06:06 +0500

    Add files via upload

--- Ecommerce Website/src/app/api/products/route.ts
+++ Ecommerce Website/src/app/api/products/route.ts
@@ -1,43 +1,11 @@
-// import { client } from "@/sanity/lib/client";
-// export const dynamic = 'force-static'
-// export async function GET() {
-//   try {
-//     const products = await client.fetch(`*[_type == "products"] {
-//   _id,
-//   title,
-//   price,
-//   priceWithoutDiscount,
-//   badge,
-//   "imageUrl": image.asset->url,
-//   category-> {
-//     _id,
-//     title
-//   },
-//   description,
-//   inventory,
-//   tags
-// }
-// `);
-
-//     return new Response(JSON.stringify(products), {
-//       headers: { "Content-Type": "application/json" },
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     return new Response(JSON.stringify({ error: "Failed to fetch products" }), {
-//       status: 500,
-//       headers: { "Content-Type": "application/json" },
-//     });
-//   }
-// }
-
 import { client } from "@/sanity/lib/client";
+import { NextRequest } from "next/server";
 
 export const dynamic = "force-static";
 
-export async function GET(req: { url: string}) {
+export async function GET(request: NextRequest) {
   try {
-    const { searchParams } = new URL(req.url);
+    const { searchParams } = new URL(request.url);
     const slug = searchParams.get("slug");
 
     if (slug) {
